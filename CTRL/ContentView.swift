0425770d5a19cfad9cb9865bbdeba7f76cb0d8ef
Hardik Kafle <hk3512@nyu.edu>
2024-12-24 18:19:41 -0500

    Alpha 1.0

--- CTRL/ContentView.swift
+++ CTRL/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world2!")
+        ZStack(alignment: .bottomLeading) { // Aligns it on the bottom
+            Color.blue.edgesIgnoringSafeArea(.all)
+            HStack(spacing: 30) { // Everything within the HStack is aligned horizontally
+                Button(action: {
+                    print("First button pressed") // This will be replaced in the future
+                }) {
+                    Image(systemName: "circle.dashed")
+                        .font(.system(size: 40))
+                        .foregroundColor(.white)
+                        .opacity(0.8)
+                        .padding()
+                }
+                Button(action: {
+                    print("Second button pressed")
+                }) {
+                    Image(systemName: "square.3.layers.3d.down.left")
+                        .font(.system(size: 40))
+                        .foregroundColor(.white)
+                        .opacity(0.8)
+                        .padding()
+                }
+                Button(action: {
+                    print("Third button pressed")
+                }) {
+                    Image(systemName: "gear")
+                        .font(.system(size: 40))
+                        .foregroundColor(.white)
+                        .opacity(0.8)
+                        .padding()
+                }
+            }
+            .padding([.leading, .bottom], 50)
         }
-        .padding()
     }
 }
 
